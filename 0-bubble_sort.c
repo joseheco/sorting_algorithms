fd546e50b3b87d16c2b5436a16c118ee0fd35d21
joseheco <jferhc@gmail.com>
2021-06-16 20:34:08 -0500

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort an array
+ * @array: pointer
+ * @size: size
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, size_copy;
+
+	size_copy = size - 1;
+	while (size_copy != 0)
+	{
+		for (i = 0; i < size_copy; i++)
+		{
+			if (array[i + 1] < array[i])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		size_copy--;
+	}
+}
